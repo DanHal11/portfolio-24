ca25cb7057525075b8393f2788ae748b6dbe704a
DanHal11 <49963663+DanHal11@users.noreply.github.com>
2024-05-16 18:59:30 -0500

    Assignment 9

---
+++ assignment9/java/script.js
@@ -0,0 +1,43 @@
+const expand_btn = document.querySelector(".expand-btn");
+
+let activeIndex;
+
+expand_btn.addEventListener("click", () => {
+    document.body.classList.toggle("collapsed");
+});
+
+const current = window.location.href;
+
+const allLinks = document.querySelectorAll(".sidebar-links a");
+
+allLinks.forEach((elem) => {
+    elem.addEventListener("click", function () {
+        const hrefLinkClick = elem.href;
+
+        allLinks.forEach((link) => {
+            if (link.href == hrefLinkClick) {
+                link.classList.add("active");
+            } else {
+                link.classList.remove("active");
+            }
+        });
+    });
+});
+
+const searchInput = document.querySelector(".search__wrapper input");
+
+searchInput.addEventListener("focus", (e) => {
+    document.body.classList.remove("collapsed");
+});
+
+
+function toggleVisibility() {
+    var element = document.getElementById('toggle-element');
+    if (element.classList.contains('hidden')) {
+        element.classList.remove('hidden');
+        element.classList.add('visible');
+    } else {
+        element.classList.remove('visible');
+        element.classList.add('hidden');
+    }
+}
